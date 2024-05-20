d9efdb1dcf742bcd93bba2410f84a934a54eb2d2
shadaf266 <shadafsiddiqui5@gmail.com>
2024-05-20 16:30:33 +0530

    Add title and favicon

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Questrial } from 'next/font/google'
+import Head from 'next/head'; // Import Head from next/head
 
 import "@fonts/css/switzer.css";
 import "./globals.css";
@@ -26,13 +27,16 @@ const secondary_font = Questrial({
 })
 
 export const metadata = {
-  title: "G2C Solutions Innovator | Quadcan Solutions Inc.",
+  title: "Voyagency - Explore the World with Unforgettable Journeys",
   description: "Quadcan Solutions Inc. is a Toronto, Canada based government-to-citizen (G2C) solutions innovator. We closely work with the government agencies to provide solutions around public security, immigration and border services, and airport passenger facilitation. All solutions focus on the providing an exceptional service level to the citizen.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${secondary_font.variable}`}>
+         <Head>
+        <link rel="icon" href="/favicon.ico" /> {/* Add this line */}
+      </Head>
       <body>
         <Preloader />
         {children}
